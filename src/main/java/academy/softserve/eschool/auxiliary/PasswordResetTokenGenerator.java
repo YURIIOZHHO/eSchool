0b8b8f07e27dff8ce62ad35e24d0baafbb029f4d
Yurii Ozhho <yura94ozhgo@gmail.com>
2026-02-12 16:17:32 +0200

    added test changes

--- src/main/java/academy/softserve/eschool/auxiliary/PasswordResetTokenGenerator.java
+++ src/main/java/academy/softserve/eschool/auxiliary/PasswordResetTokenGenerator.java
@@ -3,12 +3,12 @@ package academy.softserve.eschool.auxiliary;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class PasswordResetTokenGenerator {
-//     private static final String dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
-//             "abcdefghijklmnopqrstuvwxyz" +
-//             "0123456789";
-//     public static String generateToken() {
-//         String randomString = RandomStringUtils.random(20, dictionary);
-//         long currentTime = System.currentTimeMillis();
-//         return randomString + currentTime;
-//     }
-// }
+    private static final String dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+            "abcdefghijklmnopqrstuvwxyz" +
+            "0123456789";
+    public static String generateToken() {
+        String randomString = RandomStringUtils.random(20, dictionary);
+        long currentTime = System.currentTimeMillis();
+        return randomString + currentTime;
+    }
+}
